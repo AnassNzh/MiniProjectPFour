9ee05cb53b6592ebb24f45196cad32bf12147dcd
Anass NAZIH <anass.nazih97@gmil.com>
2019-02-03 17:33:06 +0100

    fix reload problem

--- Views/AIGameView.php
+++ Views/AIGameView.php
@@ -11,8 +11,6 @@
 <div>
     <?php
     if ($GLOBALS['turn'] === 2){
-        echo $aiMoves->AIPlay() + 1;
-
         $jouerCoup->play($aiMoves->AIPlay(), 2);
         if ($coupGagnant->is_win($aiMoves->AIPlay() + 1)) {
             echo "<b id='win'>Computer a gagné !</b><br />";
@@ -22,7 +20,6 @@
         }
     }
     else if (isset($_POST['col'])) {
-        echo $_POST['col'];
 
         // Si le coup est valide, il est joue, on verifie s'il est gagnant et on passe au tour suivant
         if ($jouerCoup->play(($_POST['col'] - 1), $GLOBALS['turn'])) {
