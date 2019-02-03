14e2cffd83d15799730e8e97d9e0f8f7e59d284d
Anass NAZIH <anass.nazih97@gmil.com>
2019-02-03 17:25:35 +0100

    fix AI problem

--- Views/AIGameView.php
+++ Views/AIGameView.php
@@ -11,26 +11,29 @@
 <div>
     <?php
     if ($GLOBALS['turn'] === 2){
+        echo $aiMoves->AIPlay() + 1;
+
         $jouerCoup->play($aiMoves->AIPlay(), 2);
-        if ($coupGagnant->is_win($aiMoves->AIPlay())) {
-            echo "<b>Computer a gagné !</b><br />";
+        if ($coupGagnant->is_win($aiMoves->AIPlay() + 1)) {
+            echo "<b id='win'>Computer a gagné !</b><br />";
             $_SESSION['finish'] = true;
         } else {
             $GLOBALS['turn'] = 1;
         }
     }
     else if (isset($_POST['col'])) {
+        echo $_POST['col'];
 
-            // Si le coup est valide, il est joue, on verifie s'il est gagnant et on passe au tour suivant
-            if ($jouerCoup->play(($_POST['col'] - 1), $GLOBALS['turn'])) {
-                if ($coupGagnant->is_win($_POST['col'])) {
-                    echo "<b>".$GLOBALS['j1']." a gagné !</b><br />";
-                    $model->incScore($GLOBALS['j1']);
-                    $_SESSION['finish'] = true;
-                } else {
-                    $GLOBALS['turn']=2;
-                }
+        // Si le coup est valide, il est joue, on verifie s'il est gagnant et on passe au tour suivant
+        if ($jouerCoup->play(($_POST['col'] - 1), $GLOBALS['turn'])) {
+            if ($coupGagnant->is_win($_POST['col'])) {
+                echo "<b id='win'>".$GLOBALS['j1']." a gagné !</b><br />";
+                $model->incScore($GLOBALS['j1']);
+                $_SESSION['finish'] = true;
+            } else {
+                $GLOBALS['turn']=2;
             }
+        }
     }
 
     $affichagePlateau->print_board();
@@ -66,9 +69,11 @@
 
 <script type="text/javascript">
     window.onload=function(){
-        var a = document.getElementById('img').src.split("/")
+        var win = document.getElementById('win');
+        var a = document.getElementById('img').src.split("/");
         a = a[a.length-1];
-        if (a == "joueur2.png")
+        console.log((a == "joueur2.png") + " and " + (win == null))
+        if (a == "joueur2.png" && win == null)
             document.forms['board'].submit();
     }
 </script>
